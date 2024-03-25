ee71c8dd840fca099619762e309751d170a15299
Jorey <jj.lensink@student.han.nl>
2024-03-25 16:21:27 +0100

    Stack implementatie

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -4,6 +4,9 @@ package nl.han.ica.icss.generator;
 import nl.han.ica.icss.ast.AST;
 
 public class Generator {
+	// maak van de decleration een string die de hele css representeert.
+	// GetString + GetString + getString ...
+	// zoals in de gramatica het opgebouwd is. declaration = property Colon expression SemiColon
 
 	public String generate(AST ast) {
         return "";
